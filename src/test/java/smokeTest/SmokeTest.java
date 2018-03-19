58d57e35912130de18b428b39b9f386c1ec80c2c
Pyranth <pyranth95@hotmail.com>
2018-03-19 19:02:49 +0100

    Created Java Maven project and added dependencies for Selenium and TestNG

---
+++ src/test/java/smokeTest/SmokeTest.java
@@ -0,0 +1,30 @@
+package smokeTest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class SmokeTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
